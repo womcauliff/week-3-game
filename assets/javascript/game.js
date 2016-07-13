96ea15323963c6a79f5377bfd7798eecaefc927f
Will McAuliff <womcauliff@gmail.com>
2016-07-12 23:40:55 -0400

    Tracks letters in secret word, determines if solved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,34 +6,67 @@ var guessTracker = {
 	guesses: [],
 
 	isNewGuess: function(unicodeLetter) {
-		console.log("isNewGuess fired");
-		//console.log(this.guesses.length);
-		// console.log(this.guesses);
-		// console.log(unicodeLetter);
-		// console.log((unicodeLetter - 65));
-		if (this.guesses[(unicodeLetter - 65)] == 0){
-			console.log("returning true");
+		if (this.guesses[(unicodeLetter-65)] == 0){
 			this.addGuess(unicodeLetter);
 			return true;
 		}
 		else {
-			console.log("returning false");
 			return false;
 		}
 	},
 
 	addGuess: function(unicodeLetter) {
-		console.log("addGuess fired");
 		this.guesses[(unicodeLetter-65)] = 1;
 	},
 
 	reset: function() {
-		console.log("reset fired");
 		this.guesses = new Array(26+1).join('0').split('').map(parseFloat);
 	}
 }
 
-function resetDisplay() {
+/**
+ * Secret Word Object
+ */
+var secretWord = {
+
+	uniques: 0,
+	uniquesFound: 0,
+	solved: false,
+	letters: [],
+
+	containsGuess: function(unicodeLetter) {
+		if (this.letters[(unicodeLetter-65)] == 1){
+			this.uniquesFound++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+
+	isSolved: function() {
+		return (this.uniques === this.uniquesFound);
+	},
+
+	reset: function(source) {
+		this.uniques = 0;
+		this.uniquesFound = 0;
+		this.letters = new Array(26+1).join('0').split('').map(parseFloat);
+		source = source.toLowerCase();
+		for (var i = 0; i < source.length; i++) {
+			var singleLetter = source[i];
+			var lookupIndex = singleLetter.charCodeAt(0) - 97;
+			if (this.letters[lookupIndex] == 0){
+				this.letters[lookupIndex] = 1;
+				this.uniques++;
+			}
+		}
+	}
+
+
+}
+
+function resetGuessDisplay() {
 	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	document.getElementById("guesses").innerHTML = "";
 }
@@ -43,9 +76,13 @@ function resetDisplay() {
  */
 var pokemon = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 var word = pokemon[0];
+
+
+secretWord.reset(word);
 guessTracker.reset();
-guessesLeft = 5;
-resetDisplay();
+
+guessesLeft = 6;
+resetGuessDisplay();
 
 document.onkeyup = function (event) {
 	var keycode = event.keyCode;
@@ -55,18 +92,28 @@ document.onkeyup = function (event) {
 		return;
 	}
 	if(guessTracker.isNewGuess(keycode)) {
-		guessesLeft--;
+
+		if (secretWord.containsGuess(keycode)){
+			//update DOM
+			console.log("good guess!");
+			if(secretWord.isSolved()) {
+				console.warn("You Win!");
+			}
+		}
+		else {
+			console.log("whoops.");
+			guessesLeft--;
+		}
+		
 		var letter = String.fromCharCode(event.keyCode).toLowerCase();
-		console.log(letter);
 		var prev = document.getElementById("guesses").innerHTML;
 		document.getElementById("guesses").innerHTML = prev + " " + letter;
 		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	}
 	if (guessesLeft == 0) {
+		console.warn("You Lose!");
 		guessTracker.reset();
-		guessesLeft = 5;
-		resetDisplay();
+		guessesLeft = 6;
+		resetGuessDisplay();
 	}
-	
 };
-
